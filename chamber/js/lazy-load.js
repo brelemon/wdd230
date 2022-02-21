7d1ad295109fef60d66904780add051160d4b2de
brelemon <97066253+brelemon@users.noreply.github.com>
2022-02-20 22:36:54 -0700

    Add files via upload

---
+++ chamber/js/lazy-load.js
@@ -0,0 +1,30 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+});
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+function loadImages(image)  {
+  console.log(image.getAttribute('data-src'))
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+}
