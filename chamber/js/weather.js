796f5af3c1e5d1a79be320b262c68e7cf5d7903c
brelemon <97066253+brelemon@users.noreply.github.com>
2022-03-19 22:03:12 -0600

    Add files via upload

---
+++ chamber/js/weather.js
@@ -0,0 +1,31 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=84062&units=imperial&appid=a5e6c79aaea5635ba7a137e07e7e5f4c';
+const imgURL = 'http://openweathermap.org/img/wn/';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+
+    const temp = jsObject.main.temp.toFixed(0);
+    const speed =jsObject.wind.speed;
+
+    document.querySelector('#temp').textContent = temp;
+    document.querySelector('#weathericon').src = imgURL+jsObject.weather[0].icon+".png";
+    document.querySelector('#condition').textContent = jsObject.weather[0].description;
+    document.querySelector("#speed").innerHTML = speed;
+
+    let t = parseFloat(temp);
+    let s = parseFloat(speed);
+    let windchill = "";  
+  
+    // Wind chill conditions are less than or equal to 50 degrees with the wind being more than 3 MPH
+    if(t <= 50 && s > 3) {
+        const v = Math.pow(s, 0.16)
+        const windchill = 35.74 + 0.6215 * t - 35.75 * v + 0.4275 * t * v;
+        document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;F`;
+    }
+    else {
+        document.querySelector("#windchill").innerHTML = "N/A";
+    }
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+});
